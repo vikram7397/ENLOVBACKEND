38654a11e1ec6f94bd41587af8b2abccaef98e9e
vikram760 <vikrambhosale219@gmail.com>
2025-12-14 17:40:17 +0530

    fix: update database connection string in connectDB function

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect('mongodb+srv://vikram7397:Vi%407397@enlov.0tyhiql.mongodb.net/enlov?retryWrites=true&w=majority');
     console.log("✅ Database connected: enlov");
   } catch (error) {
     console.error("❌ DB connection failed:", error);
